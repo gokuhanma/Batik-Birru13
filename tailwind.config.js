c31b78efce34c22e131a733b5b89412470581ff3
Ihsan Fadhlih Akbar <fdhlihhakbar@gmail.com>
2024-09-20 23:25:00 +0700

    add prize section and try font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
