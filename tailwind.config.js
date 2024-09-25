b1168d353edd907d06e20d0f1245fa26793d3414
fiqhideski <fiqhideski@gmail.com>
2024-09-25 13:37:48 +0700

    add font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  content: ['./index.html', './pages/*.html'],
+  content: [
+    './index.html', 
+    './pages/about.html',
+    './pages/olympiad.html',
+    './pages/art.html',
+    './pages/linguistic.html',
+    './pages/rb.html',
+    './pages/scout.html',
+  ],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         lexend: ['Lexend', 'sans-serif'],
         berkshire: ['Berkshire Swash', 'cursive'],
+        lato : ['Lato', 'sans-serif']
       },
       colors: {
         'greentheme': '#88ff5a',
